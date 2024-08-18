66536d75a319af4688c4c4965e66ab5cf47cc110
igorlabzun <155388424+igorlabzun@users.noreply.github.com>
2024-08-18 23:23:17 +0300

    Added class MySessionFactory

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -2,43 +2,28 @@ package com.game.repository;
 
 import com.game.entity.Player;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
-
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
-import java.util.Properties;
+
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
-    private final SessionFactory sessionFactory;
+
 
     public PlayerRepositoryDB() {
-        Properties properties = new Properties();
-        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
-        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-        properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "aSd123Rtu");
-        properties.put(Environment.HBM2DDL_AUTO, "update");
-
-        sessionFactory = new Configuration()
-                .addAnnotatedClass(Player.class)
-                .setProperties(properties)
-                .buildSessionFactory();
-
+    MySessionFactory mySessionFactory = new MySessionFactory();
+     mySessionFactory.getSessionFactory();
 
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        try(Session session = sessionFactory.openSession()) {
+        try(Session session = MySessionFactory.getSessionFactory().openSession()) {
             NativeQuery<Player> query = session.createNativeQuery("select* from rpg.player",Player.class);
             query.setFirstResult(pageNumber*pageSize);
             query.setMaxResults(pageSize);
@@ -48,7 +33,7 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @Override
     public int getAllCount() {
-        try(Session session = sessionFactory.openSession()) {
+        try(Session session = MySessionFactory.getSessionFactory().openSession()) {
             Query<Long> query = session.createNamedQuery("player_getAllCount", Long.class);
             return Math.toIntExact(query.uniqueResult());
 
@@ -57,7 +42,7 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @Override
     public Player save(Player player) {
-        try(Session session = sessionFactory.openSession()) {
+        try(Session session = MySessionFactory.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             session.save(player);
             transaction.commit();
@@ -68,7 +53,7 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @Override
     public Player update(Player player) {
-        try(Session session = sessionFactory.openSession()) {
+        try(Session session = MySessionFactory.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             session.update(player);
             transaction.commit();
@@ -79,14 +64,14 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @Override
     public Optional<Player> findById(long id) {
-        try(Session session = sessionFactory.openSession()) {
+        try(Session session = MySessionFactory.getSessionFactory().openSession()) {
             Player player = session.find(Player.class,id);
             return Optional.of(player);}
     }
 
     @Override
     public void delete(Player player) {
-        try(Session session = sessionFactory.openSession()) {
+        try(Session session =MySessionFactory.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             session.remove(player);
             transaction.commit();}
@@ -94,6 +79,6 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @PreDestroy
     public void beforeStop() {
-        sessionFactory.close();
+        MySessionFactory.getSessionFactory().close();
     }
 }
